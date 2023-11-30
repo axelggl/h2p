c574e66c70ac52b48a60fb018ed5693630e21c7e
maxel <axel.maerten@ynov.com>
2023-11-30 14:39:22 +0100

    primitive-declarations

---
+++ primitive-declarations.php
@@ -0,0 +1,8 @@
+<?php
+$firstStr = "Genre là yen a des mots";
+$firstEscapeStr = "Genre là yen \na des mots";
+$firstBool = true;
+$firstInteger = 1;
+$firstFloat = 1.01;
+$firstNull = null;
+$firstArray = array(2, null, 'Hi', 5.5, false);
