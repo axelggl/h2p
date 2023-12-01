3b674a4aed4e854a4e5788521b797693d79de562
maxel <axel.maerten@ynov.com>
2023-12-01 16:17:34 +0100

    line breaker

---
+++ line-breaker.php
@@ -0,0 +1,18 @@
+<?php
+function breakLines(string $string, int $maxLength): string {
+    $words = explode(' ', $string);
+    $lines = [];
+    $currentLine = '';
+
+    foreach($words as $word) {
+        if (strlen($currentLine) + strlen($word) + 1 <= $maxLength) {
+            $currentLine .= $word . ' ';
+        } else {
+            $lines[] = rtrim($currentLine);
+            $currentLine = $word . ' ';
+        }
+    }
+
+    $lines[] = rtrim($currentLine);
+    return implode("\n", $lines);
+}
