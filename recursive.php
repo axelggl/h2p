1e0110bbd299f56f2762f316ac0c96c67d10dd51
maxel <axel.maerten@ynov.com>
2023-12-14 15:36:31 +0100

    recursive

--- recursive.php
+++ recursive.php
@@ -2,7 +2,7 @@
 function factorial(int $arg): int | float
 {
     $factorial = 1;
-    for ($i=1; $i < $arg; $i++){
+    for ($i=1; $i <= $arg; $i++){
         $factorial = $factorial * $i;
     }
     return $factorial;
