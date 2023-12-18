056f487698d1d6a03d639150cc99d4aa7eccd881
maxel <axel.maerten@ynov.com>
2023-12-18 17:20:46 +0100

    that's anonymous

---
+++ thats-anonymous.php
@@ -0,0 +1,12 @@
+<?php
+$message = function () {
+    $date = date('F 0j, Y');
+    return "It is $date";
+};
+
+$isLeapYear = function ($year) {
+    return date('L', mktime(0, 0, 0, 1, 1, $year)) === '1';
+};
+echo $today() . "\n";
+echo $isLeapYear(2021) . "\n";
+
