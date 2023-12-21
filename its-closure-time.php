16993aa5cbf9a00d8a9a8ea8274cf0839137affc
maxel <axel.maerten@ynov.com>
2023-12-21 11:39:04 +0100

    its closure time

---
+++ its-closure-time.php
@@ -0,0 +1,15 @@
+<?php
+
+function f() {
+    return [
+        '+' => function ($a, $b) {
+            return $a + $b;
+        },
+        '-' => function ($a, $b) {
+            return $a - $b;
+        },
+        '*' => function ($a, $b) {
+            return $a * $b;
+        },
+    ];
+};
