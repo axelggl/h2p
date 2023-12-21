1e851d12e0289b32539c51c22ca21d8b517330d3
maxel <axel.maerten@ynov.com>
2023-12-21 17:31:56 +0100

    manage robot movements

--- manage-robot-movements.php
+++ manage-robot-movements.php
@@ -12,7 +12,7 @@ function manageMovements(string $str): array {
 
     for ($i = 0; $i < strlen($str); $i++) {
         if ($str[$i] == $last) {
-            $word = $tab[$str[$i]] . 'AGAIN';
+            $word = $tab[$str[$i]] . ' AGAIN';
             array_push($res, $word);
         } else {
             array_push($res, $tab[$str[$i]]);
