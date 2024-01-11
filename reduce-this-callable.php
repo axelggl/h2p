c307504891d318607e906cd029639f144fdf448d
maxel <axel.maerten@ynov.com>
2024-01-11 14:53:50 +0100

    reduce this callable

---
+++ reduce-this-callable.php
@@ -0,0 +1,11 @@
+<?php
+
+function myArrayReduce(array $array, callable $callback, $initial = null) {
+    $accumulator = $initial;
+
+    foreach ($array as $value) {
+        $accumulator = $callback($accumulator, $value);
+    }
+
+    return $accumulator;
+}
