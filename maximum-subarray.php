509f7c0076a062ad2d2caba5337facdb9eaa22ad
maxel <axel.maerten@ynov.com>
2024-01-11 14:23:10 +0100

    maximum subarray

---
+++ maximum-subarray.php
@@ -0,0 +1,13 @@
+<?php
+function findMaximumSubarray($arr) : int | float {
+    $n = count($arr);
+    if ($n === 0) {
+        return 0;
+    }
+    $maxEndingHere = $maxSoFar = $arr[0];
+    for ($i = 1; $i < $n; $i++) {
+        $maxEndingHere = max($arr[$i], $maxEndingHere + $arr[$i]);
+        $maxSoFar = max($maxSoFar, $maxEndingHere);
+    }
+    return $maxSoFar;
+}
