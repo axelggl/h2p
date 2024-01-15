5c0fe3248d450f6d0cf30892a43a09650d04ee13
maxel <axel.maerten@ynov.com>
2024-01-15 16:25:34 +0100

    many fuel

---
+++ many-fuel.php
@@ -0,0 +1,35 @@
+<?php
+
+class Car
+{
+
+    private float $tank;
+
+    public function __construct(float $tank)
+    {
+        $this->tank = 0;
+    }
+
+    public function getTank(): float
+    {
+        return $this->tank;
+    }
+
+    public function setTank(float $tank): self
+    {
+        $this->tank = $tank;
+    }
+
+    public function addFuel(float $gallons): self
+    {
+        $this->tank += $gallons;
+    }
+
+    public function ride(float $distance): self
+    {
+        $consommation = $distance / 20;
+        $this->tank -= $consommation;
+        return $this;
+    }
+
+}
