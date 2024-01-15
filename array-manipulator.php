ae4a464ae156a46848690f5ee8225329495d3df3
maxel <axel.maerten@ynov.com>
2024-01-15 09:28:18 +0100

    array manipulator

--- array-manipulator.php
+++ array-manipulator.php
@@ -1,20 +1,17 @@
 <?php
-// Reverse the order of elements in an array
+
 function reverse(array $arr): array {
     return array_reverse($arr);
 }
 
-// Stack one or more elements at the end of an array
 function push(array &$arr, ...$elements): int {
     return array_push($arr, ...$elements);
 }
 
-// Calculate the sum of values in an array
 function sum(array $arr): int {
     return array_sum($arr);
 }
 
-// Check if a value belongs to an array
 function arrayContains(array $arr, $value) {
     if (in_array($value, $arr)) {
         return $value;
@@ -23,7 +20,6 @@ function arrayContains(array $arr, $value) {
     }
 }
 
-// Merge multiple arrays into one
 function merge(array ...$arrays): array {
     return array_merge(...$arrays);
 }
